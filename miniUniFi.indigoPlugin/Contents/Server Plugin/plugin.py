166aa91593bd2271fb8fb6e56e85efee6fd8dd4b
Joe Keenan <jkeenan@Kraken.local>
2020-10-23 18:13:17 -0400

    Added UniFi device back in

--- miniUniFi.indigoPlugin/Contents/Server Plugin/plugin.py
+++ miniUniFi.indigoPlugin/Contents/Server Plugin/plugin.py
@@ -9,6 +9,13 @@ import json
 
 requests.packages.urllib3.disable_warnings(requests.packages.urllib3.exceptions.InsecureRequestWarning)
 
+# Indigo really doesn't like dicts with keys that start with a number or symbol...
+def safeKey(key):
+    if not key[0].isalpha():
+        return u'sk{}'.format(key.strip())
+    else:
+        return unicode(key.strip())
+        
 def dict_to_states(prefix, the_dict, states_list):
      for key in the_dict:
         if isinstance(the_dict[key], list):
@@ -16,7 +23,7 @@ def dict_to_states(prefix, the_dict, states_list):
         elif isinstance(the_dict[key], dict):
             dict_to_states(u"{}{}_".format(prefix, key), the_dict[key], states_list)
         elif the_dict[key]:
-            states_list.append({'key': unicode(prefix + key.strip()), 'value': the_dict[key]})
+            states_list.append({'key': unicode(safeKey(key.strip())), 'value': the_dict[key]})
 
 def list_to_states(prefix, the_list, states_list):
      for i in range(len(the_list)):
@@ -25,7 +32,7 @@ def list_to_states(prefix, the_list, states_list):
         elif isinstance(the_list[i], dict):
             dict_to_states(u"{}{}_".format(prefix, i), the_list[i], states_list)
         else:
-            states_list.append({'key': unicode(prefix + unicode(i)), 'value': the_list[i]})
+            states_list.append({'key': safeKey(unicode(i)), 'value': the_list[i]})
    
 
 ################################################################################
@@ -60,6 +67,7 @@ class Plugin(indigo.PluginBase):
 
         self.unifi_controllers = {}             # dict of controller info dicts keyed by DeviceID.  
         self.unifi_clients = {}                 # dict of device state definitions keyed by DeviceID.
+        self.unifi_devices = {}                 # dict of device state definitions keyed by DeviceID.
         self.update_needed = False
         self.last_controller = None
         self.last_site = u'default'
@@ -90,6 +98,12 @@ class Plugin(indigo.PluginBase):
                     for clientID in self.unifi_clients:
                         self.updateUniFiClient(indigo.devices[clientID])
                         
+                    # now update all the UniFi devices  
+
+                    self.logger.debug(u"Updating UniFi Devices")
+                    for deviceID in self.unifi_devices:
+                        self.updateUniFiDevice(indigo.devices[deviceID])
+                        
                 self.sleep(1.0)
 
         except self.StopThread:
@@ -106,9 +120,13 @@ class Plugin(indigo.PluginBase):
                 self.last_controller = unicode(device.id)
             
         elif device.deviceTypeId in ['unifiClient', 'unifiWirelessClient']:
-            self.unifi_clients[device.id] = None        # discovered states for the device
+            self.unifi_clients[device.id] = None                        # discovered states for the device
             self.update_needed = True
                 
+        elif device.deviceTypeId == 'unifiDevice':
+            self.unifi_devices[device.id] = None                        # discovered states for the device
+            self.update_needed = True
+
     def deviceStopComm(self, device):
 
         self.logger.info(u"{}: Stopping Device".format(device.name))
@@ -119,6 +137,8 @@ class Plugin(indigo.PluginBase):
         elif device.deviceTypeId in ['unifiClient', 'unifiWirelessClient']:
             del self.unifi_clients[device.id]
 
+        elif device.deviceTypeId == 'unifiDevice':
+            del self.unifi_devices[device.id]
 
     ########################################
     #
@@ -164,17 +184,19 @@ class Plugin(indigo.PluginBase):
                 login_url = "{}/api/login"
                 sites_url = "{}/api/self/sites"     
                 active_url = "{}/api/s/{}/stat/sta"
-                
+                device_url = "{}/api/s/{}/stat/device"
                                          
             elif device.pluginProps['controllerType'] == "UDM":
                 login_url = "{}/api/login"
                 sites_url = "{}/api/self/sites"     
                 active_url = "{}/api/s/{}/stat/sta"
+                device_url = "{}/api/s/{}/stat/device"
             
             elif device.pluginProps['controllerType'] == "UDMPro":
                 login_url = "{}/proxy/network/api/auth/login"
                 sites_url = "{}/proxy/network//api/self/sites"     
                 active_url = "{}/proxy/network/api/s/{}/stat/sta"
+                device_url = "{}/proxy/network/api/s/{}/stat/device"
             
             else:
                 self.logger.error(u"UniFi Unknown Controller Type Error: {}".format(device.pluginProps['controllerType']))
@@ -238,6 +260,25 @@ class Plugin(indigo.PluginBase):
                     actives[client['mac']] = client
                 sites[site['name']]['actives'] = actives
                 
+               
+                # Get UniFi Devices for the site
+    
+                url = device_url.format(base_url, site['name'])
+                response = session.get(url, headers=headers, verify=False)
+                if not response.status_code == requests.codes.ok:
+                    self.logger.error("UniFi Controller Get Devices Error: {}".format(response.status_code))
+                response.raise_for_status()
+
+                api_data = response.json()
+                self.logger.threaddebug("Devices response =\n{}".format(json.dumps(api_data, indent=4, sort_keys=True)))
+
+                responseList = api_data['data']
+                uDevices = {}            
+                for uDevice in responseList:
+                    self.logger.threaddebug("Saving UniFi device {} - {} ({})".format(uDevice['name'], uDevice['ip'], uDevice['mac']))
+                    uDevices[uDevice['mac']] = uDevice
+                sites[site['name']]['devices'] = uDevices
+
         # all done, save the data
         
         self.unifi_controllers[device.id]['sites'] = sites
@@ -263,7 +304,7 @@ class Plugin(indigo.PluginBase):
         
         states_list = []
         if client_data:
-            dict_to_states(u"c_", client_data, states_list)      
+            dict_to_states(u"", client_data, states_list)      
     
         self.unifi_clients[device.id] = states_list
         device.stateListOrDisplayStateIdChanged()
@@ -293,6 +334,41 @@ class Plugin(indigo.PluginBase):
             self.logger.debug(u"{}: Unknown Device Type: {}".format(device.name, device.deviceTypeId))
 
 
+    def updateUniFiDevice(self, device):
+    
+        self.logger.debug("{}: Updating UniFi Device: {}".format(device.name, device.address))
+        
+        controller = int(device.pluginProps['unifi_controller'])        
+        site = device.pluginProps['unifi_site']        
+        uDevice = device.address
+        
+        try:
+            device_data = self.unifi_controllers[controller]['sites'][site]['devices'][uDevice]
+        except:
+            self.logger.debug(u"{}: device_data not found".format(device.name))
+            device.updateStateOnServer(key="onOffState", value=False, uiValue=u"Offline")
+            device.updateStateImageOnServer(indigo.kStateImageSel.SensorTripped)
+            return
+        
+        self.logger.threaddebug(u"device_data =\n{}".format(json.dumps(device_data, indent=4, sort_keys=True)))
+        
+        states_list = []
+        if device_data:
+            dict_to_states(u"", device_data, states_list)      
+    
+        self.unifi_devices[device.id] = states_list
+        device.stateListOrDisplayStateIdChanged()
+
+        try:     
+            device.updateStatesOnServer(states_list)
+        except TypeError as err:
+            self.logger.error(u"{}: invalid state type in states_list: {}".format(device.name, states_list))   
+        
+        self.logger.debug(u"{}: Online".format(device.name))
+        device.updateStateOnServer(key="onOffState", value=True, uiValue=u"Online")
+        device.updateStateImageOnServer(indigo.kStateImageSel.SensorOn)
+            
+
     ################################################################################
     #
     # callback for state list changes, called from stateListOrDisplayStateIdChanged()
@@ -306,6 +382,26 @@ class Plugin(indigo.PluginBase):
         if device.id in self.unifi_clients and self.unifi_clients[device.id]:
             
             for item in self.unifi_clients[device.id]:
+                key = item['key']
+                value = item['value']
+                if isinstance(value, bool):
+                    dynamic_state = self.getDeviceStateDictForBoolTrueFalseType(unicode(key), unicode(key), unicode(key))
+                    self.logger.threaddebug(u"{}: getDeviceStateList, adding Bool state {}, value {}".format(device.name, key, value))
+                elif isinstance(value, (float, int)):
+                    dynamic_state = self.getDeviceStateDictForNumberType(unicode(key), unicode(key), unicode(key))
+                    self.logger.threaddebug(u"{}: getDeviceStateList, adding Number state {}, value {}".format(device.name, key, value))
+                elif isinstance(value, (str, unicode)):
+                    dynamic_state = self.getDeviceStateDictForStringType(unicode(key), unicode(key), unicode(key))
+                    self.logger.threaddebug(u"{}: getDeviceStateList, adding String state {}, value {}".format(device.name, key, value))
+                else:
+                    self.logger.debug(u"{}: getDeviceStateList, unknown type for key = {}, value {}".format(device.name, key, value))
+                    continue
+                    
+                state_list.append(dynamic_state)
+
+        elif device.id in self.unifi_devices and self.unifi_devices[device.id]:
+            
+            for item in self.unifi_devices[device.id]:
                 key = item['key']
                 value = item['value']
                 if isinstance(value, bool):
@@ -382,9 +478,30 @@ class Plugin(indigo.PluginBase):
            for mac, data in site['actives'].iteritems() if data.get('is_wired', False) == wired
         ]
                                                 
-        self.logger.threaddebug(u"get_client_list: device_list for {} ({}) = {}".format(typeId, filter, client_list))
+        self.logger.threaddebug(u"get_client_list: client_list for {} ({}) = {}".format(typeId, filter, client_list))
         client_list.sort(key=lambda tup: tup[1])
         return client_list     
+
+    def get_device_list(self, filter="", valuesDict=None, typeId="", targetId=0):
+        self.logger.debug(u"get_client_list: typeId = {}, targetId = {}, filter = {}, valuesDict = {}".format(typeId, targetId, filter, valuesDict))
+
+        try:
+            site = self.unifi_controllers[int(valuesDict["unifi_controller"])]['sites'][valuesDict["unifi_site"]]
+        except:
+            self.logger.debug(u"get_device_list: no site specified, returning empty list")
+            return []
+
+        self.logger.debug(u"get_device_list: using site {} - {}".format(valuesDict["unifi_site"], site['description']))
+        self.last_site = valuesDict["unifi_site"]
+
+        device_list = [
+            (mac, data.get('name', data.get('hostname', '--none--')))
+           for mac, data in site['devices'].iteritems()
+        ]
+                                                
+        self.logger.threaddebug(u"get_device_list: device_list for {} ({}) = {}".format(typeId, filter, device_list))
+        device_list.sort(key=lambda tup: tup[1])
+        return device_list     
 
     # doesn't do anything, just needed to force other menus to dynamically refresh
     def menuChanged(self, valuesDict = None, typeId = None, devId = None):
@@ -397,7 +514,7 @@ class Plugin(indigo.PluginBase):
         errorsDict = indigo.Dict()
         self.logger.debug("getDeviceConfigUiValues: devId = {}, typeId = {}, pluginProps =\n{}".format(devId, typeId, pluginProps))
 
-        if typeId in ['unifiClient', 'unifiWirelessClient']:
+        if typeId != 'unifiController':
             valuesDict["unifi_controller"] = self.last_controller
             valuesDict["unifi_site"] = self.last_site
         
